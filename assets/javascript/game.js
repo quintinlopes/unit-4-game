46d5bcd6db45c1bad6109978bbd9ab81d9da4263
quintinlopes <quintinlopes@gmail.com>
2018-10-19 09:49:55 -0400

    inital site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,98 @@
-var wins = 0;
-var losses = 0;
-var iceNumbers = [];
-var total = 0;
-
-$('#wins').text(wins);
-$('#losses').text(losses);
-
 $(document).ready(function () {
     var targetNumber = Math.floor(Math.random() * 120 + 19)
     $('#compScore').text(targetNumber);
-});
 
-function numberOptions() {
-    for (var i = 0; i < 4; i++) {
-        var number = Math.floor(Math.random() * 12 + 1);
-        iceNumbers.push(number);
-    }
-}
-numberOptions();
+    var wins = 0;
+    var losses = 0;
+    var iceNumbers = [];
+    var total = 0;
 
-function reset() {
-    targetNumber = Math.floor(Math.random()* 120 + 19);
-    $('#compScore').text(targetNumber);
-    iceNumbers = [];
-    numberOptions();
-    total = 0;
-    $('#yourTotal').text(total);
-}
-
-function rich() {
-    wins++;
-    $("#win-lose").html("<h2>Winner!</h2>");
-    $("#wins").html("<h3>" + wins + "</h3>");
     $('#wins').text(wins);
-    reset();
-}
-
-function broke() {
-    losses++;
-    $("#win-lose").html("<h2>Loser!</h2>");
-    $("#losses").html("<h3>" + losses + "</h3>");
     $('#losses').text(losses);
-    reset();
-}
 
-$("#ice1").on('click', function() {
-    total = total + iceNumbers[0];
-    $("#yourTotal").text(total);
-
-    if (total == targetNumber) {
-        rich();
+    function numberOptions() {
+        for (var i = 0; i < 4; i++) {
+            var number = Math.floor(Math.random() * 12 + 1);
+            iceNumbers.push(number);
+        }
     }
-    else if (total > targetNumber){
-        broke();
-    }
+    numberOptions();
 
-})
-
-$("#ice2").on('click', function() {
-    total = total + iceNumbers[1];
-    $("#yourTotal").text(total);
-
-    if (total == targetNumber) {
-        rich();
-    }
-    else if (total > targetNumber){
-        broke();
+    function reset() {
+        targetNumber = Math.floor(Math.random()* 120 + 19);
+        $('#compScore').text(targetNumber);
+        iceNumbers = [];
+        numberOptions();
+        total = 0;
+        $('#yourTotal').text(total);
     }
 
-})
-
-$("#ice3").on('click', function() {
-    total = total + iceNumbers[2];
-    $("#yourTotal").text(total);
-
-    if (total == targetNumber) {
-        rich();
-    }
-    else if (total > targetNumber){
-        broke();
+    function rich() {
+        wins++;
+        $("#win-lose").html("<h2>Winner!</h2>");
+        $("#wins").html("<h3>" + wins + "</h3>");
+        $('#wins').text(wins);
+        reset();
     }
 
-})
-
-$("#ice4").on('click', function() {
-    total = total + iceNumbers[3];
-    $("#yourTotal").text(total);
-
-    if (total == targetNumber) {
-        rich();
+    function broke() {
+        losses++;
+        $("#win-lose").html("<h2>Loser!</h2>");
+        $("#losses").html("<h3>" + losses + "</h3>");
+        $('#losses').text(losses);
+        reset();
     }
-    else if (total > targetNumber){
-        broke();
-    }
+
+    $("#ice1").on('click', function() {
+        total = total + iceNumbers[0];
+        $("#yourTotal").text(total);
+
+        if (total == targetNumber) {
+            rich();
+        }
+        else if (total > targetNumber){
+            broke();
+        }
+
+    })
+
+    $("#ice2").on('click', function() {
+        total = total + iceNumbers[1];
+        $("#yourTotal").text(total);
+
+        if (total == targetNumber) {
+            rich();
+        }
+        else if (total > targetNumber){
+            broke();
+        }
+
+    })
+
+    $("#ice3").on('click', function() {
+        total = total + iceNumbers[2];
+        $("#yourTotal").text(total);
+
+        if (total == targetNumber) {
+            rich();
+        }
+        else if (total > targetNumber){
+            broke();
+        }
+
+    })
+
+    $("#ice4").on('click', function() {
+        total = total + iceNumbers[3];
+        $("#yourTotal").text(total);
+
+        if (total == targetNumber) {
+            rich();
+        }
+        else if (total > targetNumber){
+            broke();
+        }
+
+    });
 
 });
